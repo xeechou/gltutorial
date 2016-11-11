4536fe7ed4fa70e5ae91b3aa6cf1d4f1d830f460
Sichem Zhou <xeechou@gmail.com>
2016-11-10 20:56:59 -0500

    this should work

--- include/model.hpp
+++ include/model.hpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <utility>
 #include <map>
+#include <tuple>
 #include <string>
 
 
@@ -29,12 +30,15 @@ struct Vertex {
 	glm::vec2 TexCoords; //sizeof(glm::vec2) == 8
 };
 
+typedef struct Vertex Vertex;
+
 /** UV mapping **/
 struct Texture {
 	GLuint id;
 	std::string type; ///diffuse or specular
 	std::string path; ///The relative path to the texture
 };
+typedef struct Texture Texture;
 
 class Mesh {
 public:
@@ -105,13 +109,30 @@ class Scene;
 //by the time processing mesh, we should already have the node_id in each transform
 typedef std::pair<std::string, ModelNode> modelnode_t;
 
+enum Material_type {
+	TEX_AMBIENT_MAP,
+	TEX_DIFFUSE_MAP,
+	TEX_SPECULAR_MAP,
+	TEX_NORMAL_MAP,
+	TEX_UNKNOWN_MAP
+};
+#define NUM_MATERIAL_TYPE 4
+
+struct Tex {
+	GLint64 id;
+	Material_type type;
+};
+
 
 ///
 /// The mesh also contains a transformation, different material access as well, this is super generic
 class MeshNode {
 	friend class Scene;
 	friend class ModelNode;
-	std::vector<Vertex> vertices;
+private:
+	unsigned int model_id;
+	GLuint VAO, VBO, EBO, instanceTBO, instanceTex;
+	std::vector<struct Vertex> vertices;
 	std::vector<GLuint> indices;
 	//we should have it access to material
 	std::vector< std::pair<int, aiMatrix4x4> > transforms;
@@ -119,11 +140,28 @@ class MeshNode {
 	//The same mesh, but we want to use different materials, this could be a piece of work
 
 	//based on assimp, every mesh only use one material
-	std::vector<GLuint > materials;
-	virtual setupMesh_forshader(void);
+//	std::vector<GLuint > materials;
+	//this maynot be a good idea, but assimp only support one material at a time
+	std::vector<unsigned int>materials;
+	virtual void setupMesh_forshader(GLuint program, Scene* scene);
 
 public:
-	void draw(GLuint program, Scene* scene);
+	MeshNode() {VAO = VBO = EBO = 0;}
+	~MeshNode() {
+		if (EBO)
+			glDeleteBuffers(1, &EBO);
+		if (VBO)
+			glDeleteBuffers(1, &VBO);
+		if (instanceTBO)
+			glDeleteBuffers(1, &instanceTBO);
+		if (instanceTex)
+			glDeleteTextures(1, &instanceTex);
+		if (VAO)
+			glDeleteVertexArrays(1, &VAO);
+	}
+	//all the method involve OpenGL code should be declared as virtual
+	virtual void draw(GLuint program, Scene* scene);
+	virtual void drawInstanced(GLuint program, Scene* scene);
 };
 
 
@@ -147,10 +185,11 @@ public:
 	void setParent(unsigned int parent_id);
 	void processNode(aiNode *ainode, const aiScene *aiscene, Scene *scene,
 			 //data terms
+			 unsigned int model_id,
 			 const std::string& directory, const aiMatrix4x4& transform);
 };
 
-
+typedef std::tuple<std::vector<unsigned int>, unsigned int> model_ind_t;
 /**
  * @brief The basic Scene data structure
  *
@@ -165,15 +204,22 @@ class Scene {
 	friend class MeshNode;
 	
 private:
+
+	//data
 	std::vector<struct light> light_sources;
 	std::vector<std::pair<std::string, ModelNode> > nodes;	///< all the child nodes info is here
-
-	std::vector<unsigned int> children;	///< child nodes, first level.
-	
-	//the mesh and alongside offset infomation, so we can do the instancing for it
 	std::vector<MeshNode> meshes;
-//	std::string directories;
-	std::map<std::string, Texture> textures_loaded;
+
+	//materials is a group of textures, we would have many diffuse/specular map
+	std::vector<std::pair<std::string, struct Tex> > materials;
+
+
+	//! First level node is different than others
+	std::vector<std::tuple<std::vector<unsigned int>, //mesh index for the model
+			       unsigned int> //The model id
+		    > children;
+
+	std::map<unsigned int, GLuint > instances; //this is for instancing
 
 	//the member name here is terrible, `loaded` means loaded stuff before this processingNode
 	size_t loaded_mesh, loaded_nodes, loaded_material;
@@ -185,18 +231,26 @@ private:
 		return ((mid + loaded_mesh) < this->meshes.size()) ? &this->meshes[mid+loaded_mesh]: NULL;
 	}
 protected:
-	void append_mesh(aiMesh*);
+	unsigned int getMaterialID(unsigned int aiMatID, enum Material_type type) {
+		return this->loaded_material + aiMatID * NUM_MATERIAL_TYPE + type;
+	}
+	
+	void append_mesh(aiMesh* mesh, const aiScene *scene, unsigned int model_id);
+	virtual void processingMaterial(const aiMaterial *, const std::string&);
+	virtual GLuint setupModelInstance(const std::vector<glm::mat4>& );
 public:
 	/// \brief iterate through meshes and render it.
 	Scene(void) {
-		nodes.clear(); children.clear(); meshes.clear(); textures_loaded.clear();
+		nodes.clear(); children.clear(); meshes.clear();
 		loaded_nodes = 0; loaded_material = 0; loaded_mesh = 0;
 	}
 	//after you done with loading model, you should setup the
 
-	
-	void loadModel(const char *path);
-	void draw();
+	void loadModel(const char *path, const std::vector<glm::mat4>& instances_transform);	
+	void loadModel(const char *path) {this->loadModel(path, std::vector<glm::mat4>(0));}
+
+
+	void draw(GLuint program);
 };
 
 
