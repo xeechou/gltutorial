b8b0f5159c822ce194fe88415fdfbb9cef58e59c
sichem <xeechou@gmail.com>
2017-11-13 16:23:56 -0500

    face detection work, laggy though

--- common/cvcollections/faciallandmarks.cpp
+++ common/cvcollections/faciallandmarks.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 #include <exception>
 #include <thread>
+#include <functional>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
 
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
@@ -21,6 +25,7 @@ protected:
 	cv::Rect rect_ann, rect_blob;
 	dlib::frontal_face_detector face_detector;
 	dlib::shape_predictor pose_model;
+	dlib::image_window win;
 public:
 	//or we can do the samething like with the shader, but not really a good idea, it is 1.2M
 	FacialLandmark(const std::string& landmark_path);
@@ -38,11 +43,12 @@ FacialLandmark::FacialLandmark(const std::string& landmark_path)
 	dlib::deserialize(landmark_path) >>  pose_model;
 	this->face_detector = dlib::get_frontal_face_detector();
 	//since it has a frontal face detector, I guess the process should be easy enough
+
 }
 
 FacialLandmark::~FacialLandmark()
 {
-	
+
 }
 
 void
@@ -54,21 +60,61 @@ FacialLandmark::getPoseModel(cv::Mat &frame)
 	//we just use the first face
 	if (faces.size() > 0) {
 		detected = this->pose_model(cimg, faces[0]);
+		win.clear_overlay();
+		win.set_image(cimg);
+		win.add_overlay(dlib::render_face_detections(detected));
 	}
-	
-	dlib::image_window win;
-	win.clear_overlay();
-	win.set_image(cimg);
-	win.add_overlay(dlib::render_face_detections(detected));
-	int c;
-	std::cin >> c;
+}
+
+
+//in this every special case a 40ms timer will start increase the semaphore(data_avaliable).
+//the first thread is never get locked by others. It only wait for 40ms then increase a lock of others
+
+static std::condition_variable CV;
+static std::mutex TMTX;
+static bool nextFrame;
+static bool quit=false;
+static std::string landmark_file;
+
+bool worker(void)
+{
+	FacialLandmark face_tracker(landmark_file);
+	cv::VideoCapture cap;
+	if(cap.open(0) == false) {
+		std::cout << "never got a chance to execute" << std::endl;
+		return false;
+	}
+	cv::namedWindow("cap");
+
+	while(!quit) {
+		std::unique_lock<std::mutex> lk(TMTX);
+		CV.wait(lk);
+		//now we have the data
+		nextFrame = false;
+		lk.unlock();
+		//no need to notify, I am the only dude do this now
+		cv::Mat img;
+		cap >> img;
+		face_tracker.getPoseModel(img);
+	}
 }
 
 int main(int argc, char *argv[])
 {
-	cv::Mat img = cv::imread(argv[1]);
-	FacialLandmark face_tracker(argv[2]);
-	face_tracker.getPoseModel(img);
-	
+	int manytimes=1000000;
+	//create our thread as well
+	std::thread twork(worker);
+	landmark_file = argv[1];
+
+	while(manytimes-- > 0) {
+		{
+			std::lock_guard<std::mutex> lk(TMTX);
+			nextFrame = true;
+			CV.notify_all();
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(40));
+		//std::cout << "sleeped" << std::endl;
+	}
+	quit = true;
 	return 0;
 }
