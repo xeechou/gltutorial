1618d5a3587752db998cd8fe2c6e6fdabbe826b7
Sichem Zhou <xeechou@gmail.com>
2017-11-13 21:47:33 -0500

    collision

--- common/cvcollections/faciallandmarks.cpp
+++ common/cvcollections/faciallandmarks.cpp
@@ -2,7 +2,9 @@
 #include <vector>
 #include <exception>
 #include <thread>
+#include <functional>
 
+#include <opencv2/videoio.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/objdetect.hpp>
@@ -13,6 +15,9 @@
 #include <dlib/image_processing.h>
 #include <dlib/image_processing/render_face_detections.h>
 #include <dlib/gui_widgets.h>
+
+
+#include <context.hpp>
 
 class FacialLandmark {
 	//you need to thread this thing. Because you need to process detect the
@@ -42,7 +47,7 @@ FacialLandmark::FacialLandmark(const std::string& landmark_path)
 
 FacialLandmark::~FacialLandmark()
 {
-	
+
 }
 
 void
@@ -55,7 +60,7 @@ FacialLandmark::getPoseModel(cv::Mat &frame)
 	if (faces.size() > 0) {
 		detected = this->pose_model(cimg, faces[0]);
 	}
-	
+
 	dlib::image_window win;
 	win.clear_overlay();
 	win.set_image(cimg);
@@ -64,11 +69,56 @@ FacialLandmark::getPoseModel(cv::Mat &frame)
 	std::cin >> c;
 }
 
+#include <thread>
+#include <chrono>
+#include <mutex>
+
+
+//for one thing, I can use template and pluging some of function. But it is useless, because
+class ThreadedContext : public context {
+public:
+
+};
+
+class Timer {
+protected:
+	int _ms;
+	void timer(void) {
+		while(true) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(this->_ms));
+			std::cout << "the timer is ticking" << std::endl;
+		}
+	}
+
+	std::shared_ptr<std::thread> time_thread;
+
+public:
+	Timer(unsigned int ms) {
+		_ms = ms;
+		this->time_thread = std::make_shared<std::thread>(std::bind(&Timer::timer, this));
+		//looks like this will be very specific
+		//you will have to use
+	}
+	~Timer(void) {
+		this->time_thread->join();
+	}
+
+	//I can friend myself
+//	friend void Timer::timer(Timer *th);
+};
+
+
 int main(int argc, char *argv[])
 {
-	cv::Mat img = cv::imread(argv[1]);
-	FacialLandmark face_tracker(argv[2]);
-	face_tracker.getPoseModel(img);
-	
+
+	cv::VideoCapture cap;
+//	if (!cap.open(0))
+//		return -1;
+//	std::thread t1(timer, 40);
+//	t1.join();
+//	cv::Mat img = cv::imread(argv[1]);
+//	FacialLandmark face_tracker(argv[2]);
+//	face_tracker.getPoseModel(img);
+
 	return 0;
 }
